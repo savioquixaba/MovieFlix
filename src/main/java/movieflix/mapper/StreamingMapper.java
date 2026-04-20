38182237593d95c2727e7052c7bfde27edc9ddd1
savioquixaba <savioquixaba1@hotmail.com>
2026-04-20 10:14:40 -0300

    feat: cria mapper da entidade Streaming

---
+++ src/main/java/movieflix/mapper/StreamingMapper.java
@@ -0,0 +1,21 @@
+package movieflix.mapper;
+
+import lombok.experimental.UtilityClass;
+import movieflix.controller.request.StreamingRequest;
+import movieflix.controller.response.StreamingResponse;
+import movieflix.entity.Streaming;
+
+@UtilityClass
+public class StreamingMapper {
+    public static Streaming toStreaming(StreamingRequest streamingRequest){
+        return Streaming.builder()
+                .name(streamingRequest.name())
+                .build();
+    }
+    public static StreamingResponse toCategoryResponse(Streaming streaming){
+        return StreamingResponse.builder()
+                .id(streaming.getId())
+                .name(streaming.getName())
+                .build();
+    }
+}
