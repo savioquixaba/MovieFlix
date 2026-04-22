aaaa1fcfbb4ae55a83827d24827c2bd76b80f7ab
savioquixaba <savioquixaba1@hotmail.com>
2026-04-22 13:20:33 -0300

    chore: adiciona notation de Repository

--- src/main/java/movieflix/repository/StreamingRepository.java
+++ src/main/java/movieflix/repository/StreamingRepository.java
@@ -2,6 +2,8 @@ package movieflix.repository;
 
 import movieflix.entity.Streaming;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StreamingRepository extends JpaRepository<Streaming, Long> {
 }
