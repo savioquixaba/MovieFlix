7d8a2d3f36e6ef5ddba2cf98273069c5bf69db2a
savioquixaba <savioquixaba1@hotmail.com>
2026-04-29 20:52:43 -0300

    feat: cria service do Authentication

---
+++ src/main/java/movieflix/service/AuthService.java
@@ -0,0 +1,12 @@
+package movieflix.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AuthService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
